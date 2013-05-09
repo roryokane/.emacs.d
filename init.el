253f3f8a25035bbc20562acdcdaace3afc990f75
Rory O’Kane <rory@roryokane.com>
2013-05-09 11:34:47 -0400

    enable org-src-preserve-indentation

--- init.el
+++ init.el
@@ -42,7 +42,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(org-src-preserve-indentation t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

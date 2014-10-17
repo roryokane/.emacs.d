95193d3c93284d66ff970843237126695e9efbbf
Rory O’Kane <rory@roryokane.com>
2014-10-17 01:15:26 -0400

    Disable beeping (the alarm bell)
    I considered disabling just certain behaviors with the code from “Reduce the number of warnings” on [EmacsWiki: Alarm Bell](http://www.emacswiki.org/emacs/AlarmBell) and [Emacs: disable beep when trying to move beyond the end of the document](http://stackoverflow.com/a/11679758/578288). But I looked at the list of cases in that Stack Overflow question, and I don’t want a beep for any of them – those situations are common, expected situations, not ones I’ll have to be warned about. So I’ll just turn beeping off.

--- init.el
+++ init.el
@@ -61,6 +61,8 @@
 (setq interprogram-paste-function 'ns-get-pasteboard)
 
 
+(setq ring-bell-function 'ignore)
+
 ; org-mode configuration
 (setq org-startup-indented t)
 ; TODO fix that dedenting subtree (org-shiftmetaleft) can cause it to aggregate headlines below it – subtrees should move below existing complete trees when dedenting
